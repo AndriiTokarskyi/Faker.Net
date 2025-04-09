a09376e17fa3d0170bda1732f72080273b4a4c26
NoReply Amazon <no-reply@amazon.com>
2025-04-09 21:14:05 +0000

    [Commit] Iteration 491 #errors = 53 <= 49.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs][CS0234][5,26] The type or namespace name 'VisualStudio' does not exist in the namespace 'Microsoft' (are you missing an assembly reference?)``.

--- Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
+++ Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
@@ -2,20 +2,19 @@ using Faker.Locales;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
-using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.LocaleTest
 {
-    [MSTest.TestClass]
+    [TestClass]
     public class LocaleFactoryTest
     {
-        [MSTest.TestMethod]
+        [TestMethod]
         public void TestCreate()
         {
             Type localeFactoryType = typeof(En).Assembly.GetType("Faker.LocaleFactory");
             MethodInfo createMethod = localeFactoryType.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
             object result = createMethod.Invoke(null, new object[] { LocaleType.en });
-            MSTest.Assert.AreEqual(typeof(En), result.GetType());
+            Assert.AreEqual(typeof(En), result.GetType());
         }
     }
 }
