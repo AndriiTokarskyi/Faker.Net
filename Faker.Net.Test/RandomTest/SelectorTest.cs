863aa3cc511fe0de57a08005202a171086b46ba1
NoReply Amazon <no-reply@amazon.com>
2025-04-09 14:28:26 +0000

    [Commit] Iteration 42 #errors = 87 <= 87.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/RandomTest/SelectorTest.cs][CS0234][4,17] The type or namespace name 'VisualStudio' does not exist in the namespace 'Microsoft' (are you missing an assembly reference?)``.

--- Faker.Net.Test/RandomTest/SelectorTest.cs
+++ Faker.Net.Test/RandomTest/SelectorTest.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Faker.Random;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
 
 namespace Faker.Net.Test.RandomTest
 {
