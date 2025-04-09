47617428a9a76721f3abfa1a4297bd43f8326ee4
NoReply Amazon <no-reply@amazon.com>
2025-04-09 17:41:39 +0000

    Revert "[Commit] Iteration 247 #errors = 87 <= 87."
    This reverts commit 1ca8e4232e216a565853ea8cfab0d63523f645f5.

--- Faker.Net.Test/FinanceTests.cs
+++ Faker.Net.Test/FinanceTests.cs
@@ -11,7 +11,7 @@ namespace Faker.Tests
         public void GetCurrencyNameTest()
         {
             var currencyName = Finance.Default.GetCurrencyName();
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(string.IsNullOrEmpty(currencyName));
+            Assert.IsFalse(string.IsNullOrEmpty(currencyName));
         }
     }
 }
