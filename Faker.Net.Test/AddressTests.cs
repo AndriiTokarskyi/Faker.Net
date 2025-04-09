628f12cbbf69dee6a5df7bb4c4e765305f07387c
NoReply Amazon <no-reply@amazon.com>
2025-04-09 15:54:50 +0000

    [Commit] Iteration 129 #errors = 90 <= 87.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/AddressTests.cs][CS0246][16,10] The type or namespace name 'TestMethod' could not be found (are you missing a using directive or an assembly reference?)``.

--- Faker.Net.Test/AddressTests.cs
+++ Faker.Net.Test/AddressTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Tests
 {
@@ -10,19 +10,23 @@ namespace Faker.Tests
         {
             Address address = new Address();
             var name = address.GetStreetName();
-
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(name);
         }
 
         [TestMethod]
         public void GetCityNameTest()
         {
-
+            Address address = new Address();
+            var cityName = address.GetCityName();
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(cityName);
         }
 
         [TestMethod]
         public void GetCityPrefixTest()
         {
-
+            Address address = new Address();
+            var cityPrefix = address.GetCityPrefix();
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(cityPrefix);
         }
     }
 }
