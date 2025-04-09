a8b39f9a8e00280830cce20b212b005ad91355df
NoReply Amazon <no-reply@amazon.com>
2025-04-09 21:17:34 +0000

    Revert "[Commit] Iteration 494 #errors = 53 <= 49."
    This reverts commit a37c2594d964118ae7cd3893b8a45c6853635279.

--- Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
+++ Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
@@ -1,20 +1,21 @@
 using Faker.Locales;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.LocaleTest
 {
-    [TestClass]
+    [MSTest.TestClass]
     public class LocaleFactoryTest
     {
-        [TestMethod]
+        [MSTest.TestMethod]
         public void TestCreate()
         {
             Type localeFactoryType = typeof(En).Assembly.GetType("Faker.LocaleFactory");
             MethodInfo createMethod = localeFactoryType.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
             object result = createMethod.Invoke(null, new object[] { LocaleType.en });
-            Assert.AreEqual(typeof(En), result.GetType());
+            MSTest.Assert.AreEqual(typeof(En), result.GetType());
         }
     }
 }
