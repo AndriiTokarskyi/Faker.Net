3e06e216ea2190bb2b831212681aed275e0e273d
NoReply Amazon <no-reply@amazon.com>
2025-04-09 17:28:34 +0000

    [Commit] Iteration 230 #errors = 88 <= 87.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/RandomTest/RandomFactoryTest.cs][CS0246][80,10] The type or namespace name 'TestMethodAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- Faker.Net.Test/RandomTest/RandomFactoryTest.cs
+++ Faker.Net.Test/RandomTest/RandomFactoryTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Faker.Locales;
 using Faker.Random;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.RandomTest
 {
