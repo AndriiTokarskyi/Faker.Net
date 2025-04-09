21c0a142ee39801a8196e0fc3f2844c9b3237388
NoReply Amazon <no-reply@amazon.com>
2025-04-09 16:20:19 +0000

    Revert "[Commit] Iteration 159 #errors = 90 <= 87."
    This reverts commit 2bad40f81e579fa750569466af51dcb2b488cbcf.

--- Faker.Net.Test/AddressTests.cs
+++ Faker.Net.Test/AddressTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Tests
 {
@@ -10,23 +10,19 @@ namespace Faker.Tests
         {
             Address address = new Address();
             var name = address.GetStreetName();
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(name);
+
         }
 
         [TestMethod]
         public void GetCityNameTest()
         {
-            Address address = new Address();
-            var cityName = address.GetCityName();
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(cityName);
+
         }
 
         [TestMethod]
         public void GetCityPrefixTest()
         {
-            Address address = new Address();
-            var cityPrefix = address.GetCityPrefix();
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(cityPrefix);
+
         }
     }
 }
