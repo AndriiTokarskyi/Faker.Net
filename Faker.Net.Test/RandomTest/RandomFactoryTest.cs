3b0c208b31fa610a8089e8b8423a1da27e0b6cfc
NoReply Amazon <no-reply@amazon.com>
2025-04-09 22:32:43 +0000

    [Commit] Iteration 579 #errors = 27 <= 32.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/RandomTest/RandomFactoryTest.cs][CS0246][29,10] The type or namespace name 'TestMethod' could not be found (are you missing a using directive or an assembly reference?)``.

--- Faker.Net.Test/RandomTest/RandomFactoryTest.cs
+++ Faker.Net.Test/RandomTest/RandomFactoryTest.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Reflection;
 using Faker.Locales;
 using Faker.Random;
+using NUnit.Framework;
 
 namespace Faker.Net.Test.RandomTest
 {
-    [TestClass]
+    [TestFixture]
     public class RandomFactoryTest
     {
-        [TestMethod]
+        [Test]
         public void TestRandomSelection()
         {
             int i = RandomProxy.Next();
@@ -26,7 +27,7 @@ namespace Faker.Net.Test.RandomTest
             System.Diagnostics.Debug.Assert(Array.Exists(d.Test1, n => n == result));
         }
 
-        [TestMethod]
+        [Test]
         public void TestRandomFillin()
         {
             int m = RandomProxy.Next();
@@ -50,7 +51,7 @@ namespace Faker.Net.Test.RandomTest
             }
         }
 
-        [TestMethod]
+        [Test]
         public void TestNextMethod1()
         {
             int m = RandomProxy.Next();
@@ -74,7 +75,7 @@ namespace Faker.Net.Test.RandomTest
             }
         }
 
-        [TestMethod]
+        [Test]
         public void TestNextMethod2()
         {
             int m = RandomProxy.Next(2, 100);
@@ -96,7 +97,7 @@ namespace Faker.Net.Test.RandomTest
             System.Diagnostics.Debug.Assert(numbers[0] != numbers[1]);
         }
 
-        [TestMethod]
+        [Test]
         public void TestFakerBaseDictionary()
         {
             Name n = new Name();
@@ -112,7 +113,7 @@ namespace Faker.Net.Test.RandomTest
             System.Diagnostics.Debug.Assert(n.GetType() == result.GetType());
         }
 
-        [TestMethod]
+        [Test]
         public void TestFakerFillIn()
         {
             string pattern = "@{Name.GetFirstName}";
