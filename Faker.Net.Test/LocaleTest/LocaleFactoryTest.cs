6adf0097bfcf7cc2d41825b34e050d4e12c96629
NoReply Amazon <no-reply@amazon.com>
2025-04-09 17:44:11 +0000

    [Commit] Iteration 251 #errors = 85 <= 86.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs][CS0122][13,48] 'LocaleFactory' is inaccessible due to its protection level``.

--- Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
+++ Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
@@ -1,5 +1,7 @@
 using Faker.Locales;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
 using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.LocaleTest
@@ -10,7 +12,10 @@ namespace Faker.Net.Test.LocaleTest
         [MSTest.TestMethod]
         public void TestCreate()
         {
-            MSTest.Assert.AreEqual(typeof(En), LocaleFactory.Create(LocaleType.en).GetType());
+            Type localeFactoryType = typeof(En).Assembly.GetType("Faker.LocaleFactory");
+            MethodInfo createMethod = localeFactoryType.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
+            object result = createMethod.Invoke(null, new object[] { LocaleType.en });
+            MSTest.Assert.AreEqual(typeof(En), result.GetType());
         }
     }
 }
