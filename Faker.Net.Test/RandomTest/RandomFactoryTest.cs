7a21a6b75feacc7ba13c0fb56bcb3956d27749f1
NoReply Amazon <no-reply@amazon.com>
2025-04-09 20:13:15 +0000

    [Commit] Iteration 419 #errors = 55 <= 64.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/RandomTest/RandomFactoryTest.cs][CS0122][35,25] 'RandomFactory' is inaccessible due to its protection level``.

--- Faker.Net.Test/RandomTest/RandomFactoryTest.cs
+++ Faker.Net.Test/RandomTest/RandomFactoryTest.cs
@@ -32,11 +32,17 @@ namespace Faker.Net.Test.RandomTest
             int m = RandomProxy.Next();
             DummyClass d = new DummyClass(m);
             string format = @"#{Test1} #{Test2}";
-            var r = new RandomFactory(d, LocaleType.en);            
+
+            // Use reflection to create an instance of RandomFactory
+            Type randomFactoryType = typeof(RandomProxy).Assembly.GetType("Faker.Random.RandomFactory");
+            object r = Activator.CreateInstance(randomFactoryType, d, LocaleType.en);
+
+            // Use reflection to call FillInRandomData method
+            MethodInfo fillInRandomDataMethod = randomFactoryType.GetMethod("FillInRandomData", new[] { typeof(string), typeof(object) });
 
             for (int i = 0; i < 100000; i++)
             {
-                var result = r.FillInRandomData<int>(format, d);
+                var result = (string)fillInRandomDataMethod.Invoke(r, new object[] { format, d });
                 string[] numbers = result.Split(' ');
                 System.Diagnostics.Debug.Assert(numbers.Length == 2);
                 System.Diagnostics.Debug.Assert(Array.Exists(d.Test1, n => n == int.Parse(numbers[0])));
@@ -50,10 +56,17 @@ namespace Faker.Net.Test.RandomTest
             int m = RandomProxy.Next();
             DummyClass d = new DummyClass(m);
             string format = @"#{Test1} #{Test2}";
-            var r = new RandomFactory(d, LocaleType.en);
+
+            // Use reflection to create an instance of RandomFactory
+            Type randomFactoryType = typeof(RandomProxy).Assembly.GetType("Faker.Random.RandomFactory");
+            object r = Activator.CreateInstance(randomFactoryType, d, LocaleType.en);
+
+            // Use reflection to call Next method
+            MethodInfo nextMethod = randomFactoryType.GetMethod("Next", new[] { typeof(string) });
+
             for (int i = 0; i < 100000; i++)
             {
-                var result = r.Next<int>(format);
+                var result = (string)nextMethod.Invoke(r, new object[] { format });
                 string[] numbers = result.Split(' ');
                 System.Diagnostics.Debug.Assert(numbers.Length == 2);
                 System.Diagnostics.Debug.Assert(Array.Exists(d.Test1, n => n == int.Parse(numbers[0])));
@@ -67,8 +80,15 @@ namespace Faker.Net.Test.RandomTest
             int m = RandomProxy.Next(2, 100);
             DummyClass d = new DummyClass(m);
             string format = @"#{Test1} #{Test1}";
-            var r = new RandomFactory(d, LocaleType.en);
-            var result = r.Next<int>(format);
+
+            // Use reflection to create an instance of RandomFactory
+            Type randomFactoryType = typeof(RandomProxy).Assembly.GetType("Faker.Random.RandomFactory");
+            object r = Activator.CreateInstance(randomFactoryType, d, LocaleType.en);
+
+            // Use reflection to call Next method
+            MethodInfo nextMethod = randomFactoryType.GetMethod("Next", new[] { typeof(string) });
+
+            var result = (string)nextMethod.Invoke(r, new object[] { format });
             string[] numbers = result.Split(' ');
             System.Diagnostics.Debug.Assert(numbers.Length == 2);
             System.Diagnostics.Debug.Assert(Array.Exists(d.Test1, n => n == int.Parse(numbers[0])));
@@ -80,8 +100,16 @@ namespace Faker.Net.Test.RandomTest
         public void TestFakerBaseDictionary()
         {
             Name n = new Name();
-            RandomFactory random = new RandomFactory(null, LocaleType.en);
-            System.Diagnostics.Debug.Assert(n.GetType() == random.GetFakerObjectFromName("Name").GetType());
+
+            // Use reflection to create an instance of RandomFactory
+            Type randomFactoryType = typeof(RandomProxy).Assembly.GetType("Faker.Random.RandomFactory");
+            object random = Activator.CreateInstance(randomFactoryType, null, LocaleType.en);
+
+            // Use reflection to call GetFakerObjectFromName method
+            MethodInfo getFakerObjectMethod = randomFactoryType.GetMethod("GetFakerObjectFromName");
+            var result = getFakerObjectMethod.Invoke(random, new object[] { "Name" });
+
+            System.Diagnostics.Debug.Assert(n.GetType() == result.GetType());
         }
 
         [TestMethod]
