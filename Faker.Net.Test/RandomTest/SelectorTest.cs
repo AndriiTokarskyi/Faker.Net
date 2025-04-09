902fde912e1e93d2dd2bf51ea33c4ee2e0734a4f
NoReply Amazon <no-reply@amazon.com>
2025-04-09 15:05:55 +0000

    [Commit] Iteration 79 #errors = 86 <= 87.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/RandomTest/SelectorTest.cs][CS0246][8,6] The type or namespace name 'TestClassAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- Faker.Net.Test/RandomTest/SelectorTest.cs
+++ Faker.Net.Test/RandomTest/SelectorTest.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Faker.Random;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.RandomTest
 {
-    [TestClass]
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
     public class SelectorTest
     {
         [TestMethod]
