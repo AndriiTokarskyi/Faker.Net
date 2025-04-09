afc857dbe221cd0c0befb0d8a7a816d93e79e38a
NoReply Amazon <no-reply@amazon.com>
2025-04-09 17:45:47 +0000

    Revert "[Commit] Iteration 253 #errors = 89 <= 85."
    This reverts commit 0891c44da1680a7204d20e8fe73470f2d9152126.

--- Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
+++ Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
@@ -2,19 +2,20 @@ using Faker.Locales;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
+using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.LocaleTest
 {
-    [TestClass]
+    [MSTest.TestClass]
     public class LocaleFactoryTest
     {
-        [TestMethod]
+        [MSTest.TestMethod]
         public void TestCreate()
         {
             Type localeFactoryType = typeof(En).Assembly.GetType("Faker.LocaleFactory");
             MethodInfo createMethod = localeFactoryType.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
             object result = createMethod.Invoke(null, new object[] { LocaleType.en });
-            Assert.AreEqual(typeof(En), result.GetType());
+            MSTest.Assert.AreEqual(typeof(En), result.GetType());
         }
     }
 }
