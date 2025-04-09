24f848175fbf51419ebd3e6fc332f3dcc6a73b8e
NoReply Amazon <no-reply@amazon.com>
2025-04-09 22:30:50 +0000

    [Commit] Iteration 577 #errors = 33 <= 32.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Faker.Net.Test/RandomTest/RandomFactoryTest.cs][CS0246][29,10] The type or namespace name 'TestMethod' could not be found (are you missing a using directive or an assembly reference?)``.

--- Faker.Net.Test/RandomTest/RandomFactoryTest.cs
+++ Faker.Net.Test/RandomTest/RandomFactoryTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Faker.Locales;
 using Faker.Random;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.RandomTest
 {
