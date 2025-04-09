1075e7a5bdb127b9f032eea4a3bd7a79081b929f
NoReply Amazon <no-reply@amazon.com>
2025-04-09 21:27:08 +0000

    Revert "[Commit] Iteration 506 #errors = 50 <= 49."
    This reverts commit 647f68a112feb6561be837b890c19c84dc9294d4.

--- Faker.Net.Test/AddressTests.cs
+++ Faker.Net.Test/AddressTests.cs
@@ -1,5 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Tests
 {
