56a80fe1a818b4768f3de7b67aa0d86fd79ad830
NoReply Amazon <no-reply@amazon.com>
2025-04-09 22:01:30 +0000

    Revert "[Commit] Iteration 546 #errors = 33 <= 32."
    This reverts commit 39ec8293cdc71bb01009fcd1bf742924e2d7f7d3.

--- Faker.Net.Test/LocaleTest/En.cs
+++ Faker.Net.Test/LocaleTest/En.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.LocaleTest
 {
