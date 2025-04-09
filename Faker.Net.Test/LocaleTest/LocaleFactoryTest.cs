3f91d625cd81a3aecc63fa067f3e7e884d6ffd96
NoReply Amazon <no-reply@amazon.com>
2025-04-09 14:15:27 +0000

    Revert "[Commit] Iteration 29 #errors = 88 <= 87."
    This reverts commit 51ec1007ae8b2ea633d1343e7521fda71794aaac.

--- Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
+++ Faker.Net.Test/LocaleTest/LocaleFactoryTest.cs
@@ -1,12 +1,13 @@
 using Faker.Locales;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Faker.Net.Test.LocaleTest
 {
-    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    [MSTest.TestClass]
     public class LocaleFactoryTest
     {
-        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        [MSTest.TestMethod]
         public void TestCreate()
         {
             Assert.AreEqual(typeof(En), LocaleFactory.Create(LocaleType.en).GetType());
